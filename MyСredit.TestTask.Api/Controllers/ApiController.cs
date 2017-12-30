b9b10a617bd62a1f649fa55a615e9b6e398ba4a1
Igor Nagornuy <igor.s.nagorny@gmail.com>
2017-12-30 13:00:40 +0200

    Initial commit

---
+++ MyСredit.TestTask.Api/Controllers/ApiController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyСredit.TestTask.Api.Controllers
+{
+    public class ApiController : System.Web.Http.ApiController
+    {
+        // GET api/values/5
+        public string Hi()
+        {
+            return "";
+        }
+
+        public string HiAll()
+        {
+            return "";
+        }
+
+    }
+}
---
+++ MyСredit.TestTask.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyСredit.TestTask.WebApiApplication" Language="C#" %>
