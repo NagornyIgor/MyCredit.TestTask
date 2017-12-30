57286d9181b0046c31e12215fa1c9e85967bd91f
Igor Nagornuy <igor.s.nagorny@gmail.com>
2017-12-30 13:15:23 +0200

    Services layer created

---
+++ MyCredit.TestTask.Services/Services/HelloService.cs
@@ -0,0 +1,17 @@
+﻿using MyCredit.TestTask.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCredit.TestTask.Services.Services
+{
+    public class HeloService : IGreeter
+    {
+        public string SayHello()
+        {
+            return "Hi there!";
+        }
+    }
+}
